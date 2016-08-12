bac0922ebf475cab9dfea1e41d85abc3e7807df3
Steve Mao <maochenyan@gmail.com>
2016-08-12 23:36:55 +1000

    simplify arguments

--- wrappy.js
+++ wrappy.js
@@ -17,12 +17,8 @@ function wrappy (fn, cb) {
   return wrapper
 
   function wrapper() {
-    var args = new Array(arguments.length)
-    for (var i = 0; i < args.length; i++) {
-      args[i] = arguments[i]
-    }
-    var ret = fn.apply(this, args)
-    var cb = args[args.length-1]
+    var ret = fn.apply(this, arguments)
+    var cb = arguments[arguments.length - 1]
     if (typeof ret === 'function' && ret !== cb) {
       Object.keys(cb).forEach(function (k) {
         ret[k] = cb[k]
